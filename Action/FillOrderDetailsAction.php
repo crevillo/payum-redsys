45c1a3d5e503c3a58ce8317d6fd42e42899c2ee5
Kotlyar Maksim <kotlyar.maksim@gmail.com>
2014-11-16 17:40:38 +0000

    cleanups.

--- Action/FillOrderDetailsAction.php
+++ Action/FillOrderDetailsAction.php
@@ -3,6 +3,7 @@ namespace Crevillo\Payum\Redsys\Action;
 
 use Payum\Core\Action\ActionInterface;
 use Payum\Core\ApiAwareInterface;
+use Payum\Core\Exception\LogicException;
 use Payum\Core\Exception\RequestNotSupportedException;
 use Payum\Core\Exception\UnsupportedApiException;
 use Payum\Core\Request\FillOrderDetails;
@@ -52,7 +53,6 @@ class FillOrderDetailsAction implements ActionInterface, ApiAwareInterface
         RequestNotSupportedException::assertSupports($this, $request);
 
         $order = $request->getOrder();
-        $token = $request->getToken();
         $details = ArrayObject::ensureArrayObject($order->getDetails());
 
         $details['Ds_Merchant_Amount'] = $order->getTotalAmount();
@@ -61,40 +61,29 @@ class FillOrderDetailsAction implements ActionInterface, ApiAwareInterface
         $details['Ds_Merchant_MerchantCode'] = $this->api->getMerchantCode();
         $details['Ds_Merchant_Terminal'] = $this->api->getMerchantTerminalCode();
 
-        // notification url where the bank will post the response
-        $details['Ds_Merchant_MerchantURL'] = $token->getTargetUrl();
+        if (false == $details['Ds_Merchant_MerchantURL']) {
+            if (false == $this->tokenFactory) {
+                throw new LogicException('The merchant url is not provided. You have explicitly add it to the order details or inject the token factory');
+            }
 
-        // return url in case of payment done
-        $details['Ds_Merchant_UrlOK'] = $token->getAfterUrl();
-
-        // return url in case of payment cancel. same as above
-        $details['Ds_Merchant_UrlKO'] = $token->getAfterUrl();
-
-        if (!$details['Ds_Merchant_TransactionType']) {
-            $details['Ds_Merchant_TransactionType'] = $this->api->getTransactionType();
+            $details['Ds_Merchant_MerchantURL'] = $this->tokenFactory->createNotifyToken(
+                $request->getToken()->getPaymentName(),
+                $request->getToken()->getDetails()
+            )->getTargetUrl();
         }
 
-        // set customer language to spanish in case not provided
-        if (!$details['Ds_Merchant_ConsumerLanguage']) {
-            $details['Ds_Merchant_ConsumerLanguage'] = $this->api->getDefaultLanguageCode();
+
+        if (false == $details['Ds_Merchant_TransactionType']) {
+            $details['Ds_Merchant_TransactionType'] = Api::TRANSACTIONTYPE_AUTHORIZATION;
         }
 
-        // these following to are not mandatory. only filled if present in the
-        // order details or in the options
-        $merchantName = $this->api->getMerchantName();
-        if (!$details['Ds_Merchant_MerchantName'] && !empty($merchantName)) {
-            $details['Ds_Merchant_MerchantName'] = $merchantName;
+        if (false == $details['Ds_Merchant_ConsumerLanguage']) {
+            $details['Ds_Merchant_ConsumerLanguage'] = Api::CONSUMERLANGUAGE_SPANISH;
         }
 
-        $merchantProductDescription = $this->api->getMerchantProductDescription();
-        if (!$details['Ds_Merchant_ProductDescription'] && !empty($merchantProductDescription)) {
-            $details['Ds_Merchant_ProductDescription'] = $merchantProductDescription;
-        }
+        $details['Ds_Merchant_MerchantSignature'] = $this->api->sign($details->toUnsafeArray());
 
-        // finally build the signature
-        $details['Ds_Merchant_MerchantSignature'] = $this->api->sign($details);
-
-        $order->setDetails( $details );
+        $order->setDetails($details);
     }
 
     /**
@@ -102,6 +91,9 @@ class FillOrderDetailsAction implements ActionInterface, ApiAwareInterface
      */
     public function supports($request)
     {
-        return $request instanceof FillOrderDetails;
+        return
+            $request instanceof FillOrderDetails &&
+            $request->getToken()
+        ;
     }
 }
